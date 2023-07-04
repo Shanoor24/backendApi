8dfc405bc8a2d59196df5cafca83e8934e890895
Shanoor24 <shanoorfb2403@gmail.com>
2023-07-05 04:05:05 +0530

    Backend data

---
+++ app.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./src/dbConfig/connect")
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 8080;
+
+const users_routes = require("./src/routes/users")
+
+app.get("/", (req, res) => {
+    res.send("Hi, I am live")
+})
+
+app.use("/users", users_routes);
+
+
+const start = async() => {
+ try {
+    await connectDB(process.env.MONGODB_URL);
+    app.listen(PORT, () => {
+        console.log(`${PORT} Yes I am connected`)
+    })
+ } catch (error) {
+    console.log(error)
+ }
+}
+
+start()
+
+
